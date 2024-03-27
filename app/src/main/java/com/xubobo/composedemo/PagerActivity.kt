f84addc03ae192af8a7562e692d252a50d51cb93
xubobo <henghenghalala@163.com>
2024-03-27 11:12:11 +0800

    pager dribbleEffect

--- app/src/main/java/com/xubobo/composedemo/PagerActivity.kt
+++ app/src/main/java/com/xubobo/composedemo/PagerActivity.kt
@@ -6,26 +6,43 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.PagerDefaults
 import androidx.compose.foundation.pager.PagerSnapDistance
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.rounded.PlayArrow
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.elevatedCardColors
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
@@ -52,13 +69,119 @@ class PagerActivity : ComponentActivity() {
                         HeightSpacer(value = 12.dp)
 
                         HorizontalPagerDemo()
-
+                        DribbleInspirationPager()
                     }
                 }
             }
         }
     }
 
+
+}
+
+/**
+ *https://dribbble.com/shots/17117814--Drag-This-audio-player-prototype?source=post_page-----12b3b69af2cc--------------------------------
+ */
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun DribbleInspirationPager() {
+    val img = remember {
+        listOf(
+            R.drawable.pexels,
+            R.drawable.ava,
+            R.drawable.xiaohe
+        )
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .background(MaterialTheme.colorScheme.errorContainer)
+    ) {
+        val pagerState = rememberPagerState {
+            img.size
+        }
+
+        HorizontalPager(
+            state = pagerState,
+//            pageSize = PageSize.Fixed(100.dp),
+//            pageSpacing = 12.dp
+        ) { page ->
+            Box(modifier = Modifier.fillMaxSize()) {
+                SongInformationCard(
+                    modifier = Modifier
+                        .padding(32.dp)
+                        .align(Alignment.Center),
+                    pagerState = pagerState,
+                    page = page,
+                    data = img[page]
+                )
+            }
+        }
+    }
+
+
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun SongInformationCard(modifier: Modifier, pagerState: PagerState, page: Int, data: Any) {
+
+    val offsetFraction = pagerState.getOffsetFractionForPage(page).absoluteValue.coerceIn(0f, 1f)
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .shadow(16.dp, ambientColor = Color.LightGray),
+        shape = RoundedCornerShape(32.dp),
+        colors = elevatedCardColors(containerColor = Color.White)
+    ) {
+        Column {
+            LoadImage(
+                data = data,
+                modifier = Modifier
+                    .padding(32.dp)
+                    .height(100.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color.LightGray)
+                    .graphicsLayer {
+                        val scale = lerp(1f, 1.75f, offsetFraction)
+                        // apply the scale equally to both X and Y, to not distort the image
+                        scaleX = scale
+                        scaleY = scale
+                    },
+                clip = false
+            )
+            DragToListen(offsetFraction)
+        }
+
+    }
+
+}
+
+@Composable
+fun DragToListen(offsetFraction: Float) {
+    Box(
+        modifier = Modifier
+            .height(100.dp * (1 - offsetFraction))
+            .fillMaxWidth()
+            .graphicsLayer {
+                alpha = 1 - offsetFraction
+            }
+    ) {
+        Column(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Icon(imageVector = Icons.Filled.PlayArrow, contentDescription = "")
+
+            Text("DRAG TO LISTEN")
+            Spacer(modifier = Modifier.size(4.dp))
+        }
+    }
 
 }
 
@@ -108,22 +231,20 @@ private fun HorizontalPagerDemo() {
             .fillMaxWidth()
     ) {
         CardItem(data = urls[it], modifier = Modifier.graphicsLayer {
-            val pageOffset = (
-                    (pagerState.currentPage - it) + pagerState
-                        .currentPageOffsetFraction
-                    ).absoluteValue
+
+            val pageOffsetFraction = pagerState.getOffsetFractionForPage(it).absoluteValue
 
             val scale = lerp(
                 start = 0.8f,
                 stop = 1f,
-                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                fraction = 1f - pageOffsetFraction.coerceIn(0f, 1f)
             )
 
             // We animate the alpha, between 50% and 100%
             alpha = lerp(
                 start = 0.5f,
                 stop = 1f,
-                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                fraction = 1f - pageOffsetFraction.coerceIn(0f, 1f)
             )
             scaleX = scale
             scaleY = scale
